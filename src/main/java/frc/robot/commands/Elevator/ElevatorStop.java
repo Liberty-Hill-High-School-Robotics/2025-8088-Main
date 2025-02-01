755fe87a58d59c01c9c6512a1e294a88dc26c337
Jtwerp <114360534+Jtwerp@users.noreply.github.com>
2025-02-01 12:31:00 -0600

    Finish Making Comands

---
+++ src/main/java/frc/robot/commands/Elevator/ElevatorStop.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.Coral;
+import frc.robot.subsystems.Coral;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * A simple command that grabs a hatch with the {@link HatchSubsystem}. Written explicitly for
+ * pedagogical purposes. Actual code should inline a command this simple with {@link
+ * edu.wpi.first.wpilibj2.command.InstantCommand}.
+ */
+public class IntakeIn extends Command {
+  // The subsystem the command runs on
+  private final Coral m_coral;
+
+  public IntakeIn(Coral subsystem) {
+    m_coral = subsystem;
+    addRequirements(m_coral);
+  }
+
+  @Override
+  public void initialize() {
+   
+  }
+
+  @Override
+  public void execute(){
+
+  }
+
+  @Override 
+  public void end(boolean interrupted){
+
+   }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
